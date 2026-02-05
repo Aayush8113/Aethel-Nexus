c64e460b05d417419b7e51bc07029b0572857a22
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-05 12:30:58 +0530

    feat(ui): Initialize TypingIndicator component structure

---
+++ client/src/components/TypingIndicator.jsx
@@ -0,0 +1,11 @@
+const TypingIndicator = () => {
+  return (
+    <div className="flex items-center gap-1 p-4 border rounded-tl-sm bg-slate-800 rounded-2xl w-fit border-slate-700">
+      <div className="w-2 h-2 rounded-full bg-slate-400"></div>
+      <div className="w-2 h-2 rounded-full bg-slate-400"></div>
+      <div className="w-2 h-2 rounded-full bg-slate-400"></div>
+    </div>
+  );
+};
+
+export default TypingIndicator;
