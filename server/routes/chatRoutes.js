14b3d30258e34420ac20c824668188c9fbe214e9
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-07 12:31:40 +0530

    feat(api): Expose DELETE endpoint for chat management

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { generateResponse, getAllChats, getSingleChat } = require('../controllers/chatController');
+const { generateResponse, getAllChats, getSingleChat, deleteChat } = require('../controllers/chatController');
 
 const router = express.Router();
 
 router.post('/', generateResponse);
 router.get('/', getAllChats);
 router.get('/:id', getSingleChat);
+router.delete('/:id', deleteChat); // <--- New Route
 
 module.exports = router;
