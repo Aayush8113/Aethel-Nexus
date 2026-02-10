a35d5bb423ffc59b77bbba6987e94905fb2d6bef
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-10 15:58:42 +0530

    feat(backend): Configure multer middleware for image handling

---
+++ server/middleware/upload.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const path = require("path");
+
+// Set storage engine (store in memory buffer for speed)
+const storage = multer.memoryStorage();
+
+// Check file type
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png|webp/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Images Only! (jpeg, jpg, png, webp)"));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter,
+});
+
+module.exports = upload;
