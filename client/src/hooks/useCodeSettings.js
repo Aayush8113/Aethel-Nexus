6b65f6d272ee62b101907d2d5bed1144c69431d3
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-22 16:06:16 +0530

    feat(hooks):Create useCodeSettings for global code block formatting

---
+++ client/src/hooks/useCodeSettings.js
@@ -0,0 +1,8 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useCodeSettings = () => {
+  const [showLineNumbers, setShowLineNumbers] = useLocalStorage("aethel_code_lines", true);
+  const [wordWrap, setWordWrap] = useLocalStorage("aethel_code_wrap", false);
+
+  return { showLineNumbers, setShowLineNumbers, wordWrap, setWordWrap };
+};
