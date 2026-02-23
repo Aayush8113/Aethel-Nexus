64f4bb8dbd9c9e1b8cfae53ffceb4be6376e3ae6
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-23 22:46:46 +0530

    feat(hooks):Create useCustomPersonas to persist user-defined AI profiles

---
+++ client/src/hooks/useCustomPersonas.js
@@ -0,0 +1,23 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useCustomPersonas = () => {
+  const [customPersonas, setCustomPersonas] = useLocalStorage("aethel_custom_personas", []);
+
+  const addPersona = (name, description, instruction) => {
+    const newPersona = {
+      id: `custom_${Date.now()}`,
+      name,
+      description,
+      instruction,
+      isCustom: true
+    };
+    setCustomPersonas([...customPersonas, newPersona]);
+    return newPersona;
+  };
+
+  const deletePersona = (id) => {
+    setCustomPersonas(customPersonas.filter(p => p.id !== id));
+  };
+
+  return { customPersonas, addPersona, deletePersona };
+};
