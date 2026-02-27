d69ae42fd1c0432ea591dfa94e9d447f403979f5
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-27 23:23:12 +0530

    feat(hooks): Day 27 - Create hook to persist desktop sidebar layout state

---
+++ client/src/hooks/useDesktopSidebar.js
@@ -0,0 +1,15 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDesktopSidebar = () => {
+  const [isDesktopSidebarOpen, setIsDesktopSidebarOpen] = useLocalStorage("aethel_desktop_sidebar", true);
+
+  const toggleDesktopSidebar = () => {
+    setIsDesktopSidebarOpen((prev) => !prev);
+  };
+
+  return {
+    isDesktopSidebarOpen,
+    toggleDesktopSidebar,
+    setIsDesktopSidebarOpen
+  };
+};
