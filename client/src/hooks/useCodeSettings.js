10d18bc16a4a8eaec0d659df33f3dd6eb0d06194
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-28 10:00:19 +0530

    fix(hooks): Master reset of useCodeSettings state

--- client/src/hooks/useCodeSettings.js
+++ client/src/hooks/useCodeSettings.js
@@ -1,7 +1,10 @@
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useCodeSettings = () => {
-  const [showLineNumbers, setShowLineNumbers] = useLocalStorage("aethel_code_lines", true);
+  const [showLineNumbers, setShowLineNumbers] = useLocalStorage(
+    "aethel_code_lines",
+    true,
+  );
   const [wordWrap, setWordWrap] = useLocalStorage("aethel_code_wrap", false);
 
   return { showLineNumbers, setShowLineNumbers, wordWrap, setWordWrap };
