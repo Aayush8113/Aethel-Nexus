35fa9ba20f072a18411136145daa1162dc31a4d8
Aayush Tripathi <195632269+Aayush8113@users.noreply.github.com>
2026-02-28 10:01:35 +0530

    fix(hooks): Master reset of useDesktopSidebar state

--- client/src/hooks/useDesktopSidebar.js
+++ client/src/hooks/useDesktopSidebar.js
@@ -3,13 +3,7 @@ import { useLocalStorage } from "./useLocalStorage";
 export const useDesktopSidebar = () => {
   const [isDesktopSidebarOpen, setIsDesktopSidebarOpen] = useLocalStorage("aethel_desktop_sidebar", true);
 
-  const toggleDesktopSidebar = () => {
-    setIsDesktopSidebarOpen((prev) => !prev);
-  };
+  const toggleDesktopSidebar = () => setIsDesktopSidebarOpen((prev) => !prev);
 
-  return {
-    isDesktopSidebarOpen,
-    toggleDesktopSidebar,
-    setIsDesktopSidebarOpen
-  };
+  return { isDesktopSidebarOpen, toggleDesktopSidebar, setIsDesktopSidebarOpen };
 };
